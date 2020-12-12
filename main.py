ead493c7712142b8f0f5fef3fb1d7ae55e216089
benlevin0224 <71128482+benlevin0224@users.noreply.github.com>
2020-12-12 09:24:59 -0500

    Update main.py
    Cleaned some of the code to make it a bit more readable.

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 #to create and write to CSV file
-file_name = input('enter your file name here. ')+".xlsx"
+file_name = "test.xlsx"#input('enter your file name here. ')+".xlsx"
 workbook = xlsxwriter.Workbook(file_name)
 worksheet = workbook.add_worksheet()
 
 page_num = 1
 serviceurl = "https://api.scryfall.com/cards/search?&page=%s\&q=" % page_num
-card_type = input("Enter card type here. All lower case. ")
-cmdr = input("Enter commander's colors, WUBRG format. ")
+card_type = "elf"#input("Enter card type here. All lower case. ")
+cmdr = "G"#input("Enter commander's colors, WUBRG format. ")
 
 url = serviceurl + "type%%3A%s" % card_type + "+" + "id%%3A%s" % cmdr
 print("Retrieving", url)
@@ -30,40 +30,32 @@ headers = ["name","mana","oracle text"]
 for col_num, data in enumerate(headers):
     worksheet.write(0, col_num, data)
 
+column = 0
 row = 1
 while has_more is True:
     count = 0
-    column = 0
     for cards in range(175):
         try:
-            name = js["data"][count]["name"]
-            mana = js["data"][count]["mana_cost"]
-            oracle_text = js["data"][count]["oracle_text"]
-            worksheet.write(row,column,name)
-            column += 1
-            worksheet.write(row,column,mana)
-            column += 1
-            worksheet.write(row,column,oracle_text)
-            column =0
+            lst = [(js["data"][count]["name"]), (js["data"][count]["mana_cost"]), (js["data"][count]["oracle_text"])]
+            for column, value in enumerate(lst):
+                worksheet.write(row, column, value)
             row += 1
             count += 1
             sleep(0.1)
         except:
+            column = 0
             worksheet.write(row,column,js["data"][count]["name"])
             row += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][0]["name"])
-            column += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][0]["mana_cost"])
-            column += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][0]["oracle_text"].encode("UTF-8").decode())
-            row += 1
-            column = 0
-            worksheet.write(row,column,js["data"][count]["card_faces"][1]["name"])
-            column += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][1]["mana_cost"])
-            column += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][1]["oracle_text"].encode("UTF-8").decode())
+            card_side = 0
+            for card_side in range(2):
+                lst = [(js["data"][count]["card_faces"][card_side]["name"]),(js["data"][count]["card_faces"][card_side]["mana_cost"]),
+                (js["data"][count]["card_faces"][card_side]["oracle_text"].encode("UTF-8").decode())]
+                for column, value in enumerate(lst):
+                    worksheet.write(row, column, value)
+                    card_side += 1
+                row += 1
             count += 1
+
     page_num += 1
     serviceurl = "https://api.scryfall.com/cards/search?&page=%s\&q=" % page_num
     url = serviceurl + "type%%3A%s" % card_type + "+" + "id%%3A%s" % cmdr
@@ -76,33 +68,26 @@ while has_more is True:
 else:
     count = 0
     for cards in range((js["total_cards"]-(175*(page_num-1)))):
-        try: 
-            name = js["data"][count]["name"]
-            mana = js["data"][count]["mana_cost"]
-            oracle_text = js["data"][count]["oracle_text"]
-            worksheet.write(row,column,name)
-            column += 1
-            worksheet.write(row,column,mana)
-            column += 1
-            worksheet.write(row,column,oracle_text)
-            column =0
+        try:
+            lst = [(js["data"][count]["name"]), (js["data"][count]["mana_cost"]), (js["data"][count]["oracle_text"])]
+            for column, value in enumerate(lst):
+                worksheet.write(row, column, value)
             row += 1
             count += 1
             sleep(0.1)
         except:
+            column = 0
             worksheet.write(row,column,js["data"][count]["name"])
             row += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][0]["name"])
-            column += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][0]["mana_cost"])
-            column += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][0]["oracle_text"].encode("UTF-8").decode())
-            row += 1
-            column = 0
-            worksheet.write(row,column,js["data"][count]["card_faces"][1]["name"])
-            column += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][1]["mana_cost"])
-            column += 1
-            worksheet.write(row,column,js["data"][count]["card_faces"][1]["oracle_text"].encode("UTF-8").decode())
+            card_side = 0
+            for card_side in range(2):
+                lst = [(js["data"][count]["card_faces"][card_side]["name"]),(js["data"][count]["card_faces"][card_side]["mana_cost"]),
+                (js["data"][count]["card_faces"][card_side]["oracle_text"].encode("UTF-8").decode())]
+                for column, value in enumerate(lst):
+                    worksheet.write(row, column, value)
+                    card_side += 1
+                row += 1
             count += 1
+
+print('Finished exporting')
 workbook.close()
