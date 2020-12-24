a54e8dc522c58e75995e8e3230d5610a9bef1806
benlevin0224 <71128482+benlevin0224@users.noreply.github.com>
2020-12-24 08:19:19 -0500

    Update main.py
    Removed repeat code
    
    Updated the range in the for loops so
    it is dynamic
    
    Switched from urllib to GET
    
    Removed .ecode(utf-8).decode from the oracle_text
    
    It should now pull the new page from the API instead of manually updating it

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
-import json
-import ssl
-import urllib.request
 from time import sleep
 import xlsxwriter
-
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+import requests
 
 # to create and write to CSV file
-file_name = input('enter your file name here. ') + ".xlsx"
+file_name = input("enter your file name here. ") + ".xlsx"
 workbook = xlsxwriter.Workbook(file_name)
 worksheet = workbook.add_worksheet()
 
-page_num = 1
-serviceurl = "https://api.scryfall.com/cards/search?&page=%s\\&q=" % page_num
+serviceurl = "https://api.scryfall.com/cards/search?&page=1&q="
 card_type = input("Enter card type here. All lower case. ")
 cmdr = input("Enter commander's colors, WUBRG format. ")
-
 url = serviceurl + "type%%3A%s" % card_type + "+" + "id%%3A%s" % cmdr
-print("Retrieving", url)
-uh = urllib.request.urlopen(url, context=ctx)
-data = uh.read().decode()
-js = json.loads(data)
-has_more = js["has_more"]
+response = requests.get(url)
+js = response.json()
 
 headers = ["name", "mana", "oracle text"]
 for col_num, data in enumerate(headers):
@@ -32,46 +20,17 @@ for col_num, data in enumerate(headers):
 
 column = 0
 row = 1
-while has_more is True:
-    count = 0
-    # Each page from the URL only pulls the first 175 cards
-    for cards in range(175):
-        try:
-            lst = [(js["data"][count]["name"]), (js["data"][count]["mana_cost"]), (js["data"][count]["oracle_text"])]
-            for column, value in enumerate(lst):
-                worksheet.write(row, column, value)
-            row += 1
-            count += 1
-            sleep(0.1)
-        # The except KeyError is to pull flip cards which are stored differently than normal cards
-        except KeyError:
-            column = 0
-            worksheet.write(row, column, js["data"][count]["name"])
-            row += 1
-            card_side = 0
-            for card_side in range(2):
-                lst = [(js["data"][count]["card_faces"][card_side]["name"]),
-                       (js["data"][count]["card_faces"][card_side]["mana_cost"]),
-                       (js["data"][count]["card_faces"][card_side]["oracle_text"].encode("UTF-8").decode())]
-                for column, value in enumerate(lst):
-                    worksheet.write(row, column, value)
-                    card_side += 1
-                row += 1
-            count += 1
-    page_num += 1
-    serviceurl = "https://api.scryfall.com/cards/search?&page=%s\\&q=" % page_num
-    url = serviceurl + "type%%3A%s" % card_type + "+" + "id%%3A%s" % cmdr
-    uh = urllib.request.urlopen(url, context=ctx)
-    data = uh.read().decode()
-    js = json.loads(data)
-    has_more = js["has_more"]
-    print("Retrieving", url)
 
-else:
+while True:
     count = 0
-    for cards in range((js["total_cards"] - (175 * (page_num - 1)))):
+    print("Retrieving URL: " + url)
+    for cards in range(len(js["data"])):
         try:
-            lst = [(js["data"][count]["name"]), (js["data"][count]["mana_cost"]), (js["data"][count]["oracle_text"])]
+            lst = [
+                (js["data"][count]["name"]),
+                (js["data"][count]["mana_cost"]),
+                (js["data"][count]["oracle_text"])
+            ]
             for column, value in enumerate(lst):
                 worksheet.write(row, column, value)
             row += 1
@@ -83,14 +42,21 @@ else:
             row += 1
             card_side = 0
             for card_side in range(2):
-                lst = [(js["data"][count]["card_faces"][card_side]["name"]),
-                       (js["data"][count]["card_faces"][card_side]["mana_cost"]),
-                       (js["data"][count]["card_faces"][card_side]["oracle_text"].encode("UTF-8").decode())]
+                lst = [
+                    (js["data"][count]["card_faces"][card_side]["name"]),
+                    (js["data"][count]["card_faces"][card_side]["mana_cost"]),
+                    (js["data"][count]["card_faces"][card_side]["oracle_text"])
+                ]
                 for column, value in enumerate(lst):
                     worksheet.write(row, column, value)
                     card_side += 1
                 row += 1
             count += 1
-
-print('Finished exporting')
+    try:
+        url = js["next_page"]
+        response = requests.get(url)
+        js = response.json()
+    except KeyError:
+        break
+print("Finished exporting")
 workbook.close()
